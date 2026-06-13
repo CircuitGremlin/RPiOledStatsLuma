6312e6c868c549fad184c6c64868d5d3b609904e
Benjamin Lenherr <sunsetroad@gmx.ch>
2026-06-13 19:12:03 +0200

    Create install_repo.sh

---
+++ install_repo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get repo
+echo "Get repo"
+git clone https://github.com/blenherr/RPiOledStatsLuma.git
+
+# Open directory
+echo "Open directory"
+cd RPiOledStatsLuma
+
+# Create virtual python environment
+echo "Create virtual python environment"
+python3 -m venv --system-site-packages pyvenv
+
+# Activate virtual python environment
+echo "Activate virtual python environment"
+source ./pyvenv/bin/activate
+
+# Install pip packages
+echo "Install pip packages"
+pip3 install pyyaml psutil luma.oled
